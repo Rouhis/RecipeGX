723dbd0178eaa64dfc1fb0d3c8a7d8a25c0ad27f
AxelKah <axel.kahkonen@gmail.com>
2023-02-21 13:03:35 +0200

    Started working on profile page

--- App.js
+++ App.js
@@ -1,15 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-magnus'
-export default function App() {
+import {StyleSheet, View} from 'react-native';
+import {Avatar, Button, Div, Icon, Input, Text} from 'react-native-magnus';
+import {StatusBar} from 'expo-status-bar';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button>ASD</Button>
-    </View>
+    <Div rounded="lg" p="xl">
+      <Avatar
+        alignSelf="center"
+        size={86}
+        bg="green800"
+        rounded="lg"
+        shadow={1}
+      >
+        <Icon name="user" color="white" fontFamily="Feather" />
+      </Avatar>
+      <Text
+        fontSize="lg"
+        textAlign="center"
+        fontWeight="bold"
+        textTransform="uppercase"
+        color="red400"
+        letterSpacing={1}
+        mt="lg"
+        mb="lg"
+      >
+        ProfileName
+      </Text>
+      <Div bg="grey" p="lg" h={600} roundedTop="xl">
+        <Div row flexWrap="wrap" justifyContent="space-evenly">
+          <Button
+            mt="xs"
+            px="xs"
+            py="xs"
+            bg="transparent"
+            borderBottomColor="green500"
+            color="red500"
+            underlayColor="red100"
+          >
+            My Recipes
+          </Button>
+          <Button
+            mt="xs"
+            px="xs"
+            py="xs"
+            bg="transparent"
+            borderBottomColor="green500"
+            color="red500"
+            underlayColor="red100"
+          >
+            Favourites
+          </Button>
+        </Div>
+      </Div>
+    </Div>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +64,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ views/Home.js
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <List />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
