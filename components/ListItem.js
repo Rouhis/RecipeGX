b478f60d30ce2439b9d482acf603719d214c5161
Rouhis <eeturouhiainen1@gmail.com>
2023-02-21 19:04:18 +0200

    styling

--- components/ListItem.js
+++ components/ListItem.js
@@ -3,24 +3,42 @@ import {
     TouchableOpacity,
   } from 'react-native';
 import PropTypes from 'prop-types';
-import { Text, Image } from 'react-native-magnus';
+import { Text, Image, Div } from 'react-native-magnus';
 import { uploadsUrl } from '../utils/Variables';
+import { pink } from '../utils/Colors';
   
   const ListItem = ({singleMedia}) => {
     const item = singleMedia;
     return (
+      
       <TouchableOpacity>
+        <Div
+        w={'100%'}
+        flex={1}
+        alignItems={'center'}
+        justifyContent={'center'}
+        >
+          <Div
+          w={'80%'}
+          flex={1}
+          alignItems={'center'}
+          justifyContent={'center'}
+          bg={pink}
+          rounded="lg"
+          m={10}>
         <Image
            h={100}
-           w={100}
+           w={200}
            m={10}
-           rounded="circle"
+           roundedTop={15}
+           roundedBottom={15}
           source={{uri: uploadsUrl + item.thumbnails?.w160}}
         ></Image>
-        <View>
+        
           <Text>{item.title}</Text>
           <Text>{item.description}</Text>
-        </View>
+        </Div>
+        </Div>
       </TouchableOpacity>
     );
   };
--- views/Home.js
+++ views/Home.js
@@ -1,27 +1,43 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import { Text } from "react-native-magnus";
-import {StyleSheet, SafeAreaView} from 'react-native';
-import {StatusBar} from 'expo-status-bar';
-import List from '../components/List';
+import * as React from "react";
+import { Text, Fab , Button ,Icon , Div} from "react-native-magnus";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import List from "../components/List";
 const Home = () => {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <List />
+        <Fab bg="blue600" h={50} w={50}>
+          <Button p="none" bg="transparent" justifyContent="flex-end">
+            <Div rounded="sm" bg="white" p="sm">
+              <Text fontSize="md">Add a recipe</Text>
+            </Div>
+            <Icon
+              name="pizza-slice"
+              fontFamily="FontAwesome5"
+              color="black"
+              h={50}
+              w={50}
+              rounded="circle"
+              ml="md"
+              bg="white"
+            />
+          </Button>
+        </Fab>
       </SafeAreaView>
       <StatusBar style="auto" />
     </>
   );
-  };
+};
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
-  export default Home;
+export default Home;
